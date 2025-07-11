465b3bf4326093753d88a6b8c18a53052b270ee1
fosandon-columat <fosandon@columat.com>
2025-07-11 14:17:14 +0200

    first commit

---
+++ src/components/List.vue
@@ -0,0 +1,72 @@
+<script setup>
+    defineProps({
+        products: {
+            type: Array,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div class="card-list">
+        <div v-for="product in products" :key="product.id" class="card">
+            <!-- <img :src="product.image" :alt="product.title" /> -->
+            <div class="card-body">
+                <h3>{{ product.title }}</h3>
+                <p>{{ product.description }}</p>
+                <p>Price: ${{ product.price }}</p>
+                <p>Stock: {{ product.stock }}</p>
+                <p>Category: {{ product.category }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+    .card-list {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 20px;
+        width: 100%;
+    }
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+
+        background-color: #f9f9f9;
+        border: 1px solid #ddd;
+        border-radius: 8px;
+        padding: 10px;
+        text-align: center;
+        width: 20%;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        transition: transform 0.2s;
+        color: black;
+    }
+    .card:hover {
+        transform: translateY(-5px);
+    }
+    .card-body {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+    }
+    .card-body h3 {
+        margin: 0;
+        padding: 0;
+    }
+    .card-body p {
+        margin: 0;
+        padding: 0;
+    }
+    img {
+        width: 100%;
+        height: 150px;
+        object-fit: cover;
+        border-radius: 8px;
+        margin-bottom: 10px;
+    }
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,53 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+import List from '../components/List.vue';
+
+   const products = ref([]);
+
+   const fetchProducts = async () => {
+       try {
+           const response = await fetch('DATA/products.json')
+        //    console.log('Response status:', response);
+           if (!response.status === 200) {
+               throw new Error('Network response was not ok')
+           }
+           const productsData = await response.json();
+              console.log('Products data:', productsData.data);
+           products.value = productsData.data;
+       } catch (error) {
+           console.error('Error fetching products:', error);
+       }
+   };
+
+   onMounted(async () => {
+       await fetchProducts();
+        //  console.log('Products fetched:', products.value);
+   });
+
+
+</script>
+<template>
+    <main>
+        <h1>Welcome to the Home Page</h1>
+        <List :products="products" />
+    </main>
+</template>
+<style scoped>
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: start;
+        align-items: center;
+        min-height: 100vh;
+    }
+    .home-page {
+        text-align: center;
+        margin-top: 20px;
+    }
+    h1 {
+        color: #42b883;
+    }
+    p {
+        color: #35495e;
+    }
+</style>
